d65ca90514f367fc0a0099409d3e56cd1380e415
CafinaGH <jurdanetaportillo@gmail.com>
2024-11-04 23:48:27 -0400

    updated files

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,8 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
+    result = result.upper()
+    result = result.translate(str.maketrans({'O': '0', 'I': '1', 'A':'@'}))
+    result = list(result)
     return result  
+print(fn_hack_10())
--- hack_5.py
+++ hack_5.py
@@ -4,5 +4,6 @@ text: "fooziman" output => "f00z1m@n"
 
 def fn_hack_5():
     result = "fooziman"
-    #...
+    result = result.translate(str.maketrans({'o': '0', 'i': '1', 'a':'@'}))
     return result  
+print(fn_hack_5())
